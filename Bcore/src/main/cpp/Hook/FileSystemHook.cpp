7b79ab0eaa91fef7d66365305a6f962ab406608f
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-03-05 10:53:59 +0530

    Update FileSystemHook.cpp

--- Bcore/src/main/cpp/Hook/FileSystemHook.cpp
+++ Bcore/src/main/cpp/Hook/FileSystemHook.cpp
@@ -1,8 +1,13 @@
 #include "FileSystemHook.h"
 #include "../IO.h"
 #include "../Log.h"
+
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <dlfcn.h>
+#include <sys/stat.h>
+
 #include <cstdarg>
 #include <cerrno>
 
@@ -18,6 +23,7 @@ static int (*orig_rmdir)(const char*) = nullptr;
 
 int FileSystemHook::hook_open(const char* path, int flags, ...) {
     std::string redirected = IO::redirect(path ? path : "");
+
     mode_t mode = 0;
     if (flags & O_CREAT) {
         va_list args;
@@ -25,6 +31,7 @@ int FileSystemHook::hook_open(const char* path, int flags, ...) {
         mode = va_arg(args, mode_t);
         va_end(args);
     }
+
     return orig_open ? orig_open(redirected.c_str(), flags, mode) : -1;
 }
 
@@ -66,14 +73,19 @@ int FileSystemHook::hook_rmdir(const char* pathname) {
 
 bool FileSystemHook::install() {
     bool ok = true;
-    ok &= hookFunction((void*)open,    (void*)hook_open,    (void**)&orig_open);
-    ok &= hookFunction((void*)stat,    (void*)hook_stat,    (void**)&orig_stat);
-    ok &= hookFunction((void*)access,  (void*)hook_access,  (void**)&orig_access);
-    ok &= hookFunction((void*)opendir, (void*)hook_opendir, (void**)&orig_opendir);
-    ok &= hookFunction((void*)rename,  (void*)hook_rename,  (void**)&orig_rename);
-    ok &= hookFunction((void*)unlink,  (void*)hook_unlink,  (void**)&orig_unlink);
-    ok &= hookFunction((void*)mkdir,   (void*)hook_mkdir,   (void**)&orig_mkdir);
-    ok &= hookFunction((void*)rmdir,   (void*)hook_rmdir,   (void**)&orig_rmdir);
+
+    void* libc = dlopen("libc.so", RTLD_NOW);
+
+    ok &= hookFunction(dlsym(libc, "open"),    (void*)hook_open,    (void**)&orig_open);
+    ok &= hookFunction(dlsym(libc, "stat"),    (void*)hook_stat,    (void**)&orig_stat);
+    ok &= hookFunction(dlsym(libc, "access"),  (void*)hook_access,  (void**)&orig_access);
+    ok &= hookFunction(dlsym(libc, "opendir"), (void*)hook_opendir, (void**)&orig_opendir);
+    ok &= hookFunction(dlsym(libc, "rename"),  (void*)hook_rename,  (void**)&orig_rename);
+    ok &= hookFunction(dlsym(libc, "unlink"),  (void*)hook_unlink,  (void**)&orig_unlink);
+    ok &= hookFunction(dlsym(libc, "mkdir"),   (void*)hook_mkdir,   (void**)&orig_mkdir);
+    ok &= hookFunction(dlsym(libc, "rmdir"),   (void*)hook_rmdir,   (void**)&orig_rmdir);
+
     LOGI("FileSystemHook::install %s", ok ? "OK" : "partial");
+
     return ok;
 }
