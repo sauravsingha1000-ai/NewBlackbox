604602b68275fd576cebc2b6723991fe58b5aff3
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-03-05 20:52:20 +0530

    Update App.kt

--- app/src/main/java/top/niunaijun/blackboxa/app/App.kt
+++ app/src/main/java/top/niunaijun/blackboxa/app/App.kt
@@ -7,24 +7,25 @@ import top.niunaijun.blackboxa.util.CrashHandler
 
 class App : Application() {
 
-    companion object {
-        lateinit var instance: App
-            private set
-    }
+companion object {
+    lateinit var instance: App
+        private set
+}
 
-    override fun attachBaseContext(base: Context) {
-        super.attachBaseContext(base)
-        BlackBoxCore.get().doAttachBaseContext(base)
-    }
+override fun attachBaseContext(base: Context) {
+    super.attachBaseContext(base)
+    BlackBoxCore.get().doAttachBaseContext(base)
+}
 
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
+override fun onCreate() {
+    super.onCreate()
+    instance = this
 
-        // Enable crash logger
-        CrashHandler.getInstance().init(this)
+    // Enable crash logger
+    CrashHandler.get().init(this)
 
-        BlackBoxCore.get().doCreate(this)
-        AppManager.init(this)
-    }
+    BlackBoxCore.get().doCreate(this)
+    AppManager.init(this)
+}
+
 }
