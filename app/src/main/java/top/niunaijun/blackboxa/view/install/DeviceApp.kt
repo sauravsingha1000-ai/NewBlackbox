732d5110f47b17c0c2a63a4d0d3bfb041dbb53c5
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-03-07 01:22:41 +0530

    Create DeviceApp.kt

---
+++ app/src/main/java/top/niunaijun/blackboxa/view/install/DeviceApp.kt
@@ -0,0 +1,9 @@
+package top.niunaijun.blackboxa.view.install
+
+import android.graphics.drawable.Drawable
+
+data class DeviceApp(
+val name: String,
+val packageName: String,
+val icon: Drawable
+)
