8ba1a108bfe7008b805603b43c8dcdd401fdbd26
sauravsingha1000-ai <sauravsingha1000@gmail.com>
2026-03-07 01:21:53 +0530

    Create DeviceAppsActivity.kt

---
+++ app/src/main/java/top/niunaijun/blackboxa/view/install/DeviceAppsActivity.kt
@@ -0,0 +1,44 @@
+package top.niunaijun.blackboxa.view.install
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import top.niunaijun.blackbox.BlackBoxCore
+import top.niunaijun.blackboxa.databinding.ActivityDeviceAppsBinding
+
+class DeviceAppsActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityDeviceAppsBinding
+private val apps = mutableListOf<DeviceApp>()
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityDeviceAppsBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    loadApps()
+
+    binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+    binding.recyclerView.adapter = DeviceAppsAdapter(apps) { pkg ->
+        BlackBoxCore.get().installPackageAsUser(pkg, 0)
+    }
+}
+
+private fun loadApps() {
+
+    val pm = packageManager
+    val packages = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+
+    packages.forEach {
+
+        val label = pm.getApplicationLabel(it).toString()
+        val icon = pm.getApplicationIcon(it)
+
+        apps.add(DeviceApp(label, it.packageName, icon))
+    }
+}
+
+}
